41698c17a293d0eab72618d56c4c69cfa51533d4
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2021-02-24 20:03:24 -0300

    NLW#4 Aula03

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,16 +1,16 @@
-import styles from '../styles/components/Profile.module.css'
+import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
-  return(
+  return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/DevRafael-GL.png" alt="Rafael Gonçalves"/>
+      <img src="https://github.com/DevRafael-GL.png" alt="Rafael Gonçalves" />
       <div>
         <strong>Rafael Gonçalves</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
+          <img src="icons/level.svg" alt="Level" />
           Level 1
         </p>
       </div>
     </div>
-  )
+  );
 }
